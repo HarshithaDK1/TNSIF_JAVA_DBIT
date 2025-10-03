d8ef52dea2d6c23751b89664221373a9fd3e879c
C2TC_Java_programs <harshithadk85@gmail.com>
2025-10-03 23:16:20 +0530

    Add files via upload

---
+++ C2TC_Java_programs-main (1)/C2TC_Java_programs-main/Core_Java/src/com/tns/encapsulation/ProductDemo.java
@@ -0,0 +1,17 @@
+package com.tns.encapsulation;
+
+public class ProductDemo {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		
+		product.setProductid(1);
+		product.setProductName("Laptop");
+		product.setPrice(65676.89);
+		
+		System.out.println("Product ID :"+product.getProductid());
+		System.out.println("Product Name :"+product.getProductName());
+		System.out.println("Product Price :"+product.getPrice());
+	}
+
+}
